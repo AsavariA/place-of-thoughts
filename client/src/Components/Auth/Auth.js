edcd1d24957955ff1ddfca3a0321839bb1d5cc38
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-07-13 17:35:31 +0530

    authentication backend

--- client/src/Components/Auth/Auth.js
+++ client/src/Components/Auth/Auth.js
@@ -15,7 +15,11 @@ const Auth = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formData);
+        if (formData.password === formData.confirmPassword){
+            console.log(formData);
+        } else {
+            alert('Passwords dont match!')
+        }
     }
 
     const handleChange = (e) => {
@@ -46,7 +50,7 @@ const Auth = () => {
                             )}
                             <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
                             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-                            {isSignup && <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password" />}
+                            <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password" />
                         </Grid>
                         <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                             {isSignup ? 'Sign Up' : 'Sign In'}
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+import dotenv from 'dotenv'
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello from the place of thoughts!');
+  });
+
+// CONNECTING TO DB
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`)))
+  .catch(err => console.log(err));
+
+  mongoose.set('useFindAndModify', false);
